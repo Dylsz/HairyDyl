ac7d305516181f965162d4e7de229aed4f5cd503
HairySon <flappybird.mc@gmail.com>
2015-04-08 19:39:00 +0100

    project initialize

---
+++ src/main/java/com/gmail/flappybird/hairydyl/Main.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gmail.flappybird.hairydyl;
+
+/**
+ *
+ * @author Harrison
+ */
+public class Main {
+    
+}
